4bc05cf31b71caa6cd41b8827a77369c24a72072
Björn Andersson <ba@sanitarium.se>
2024-09-15 20:27:20 +0800

    Initial commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/gaqzi/incident-review
+module github.com/gaqzi/incident-reviewer
 
 go 1.23.1
+
+require github.com/donseba/go-htmx v1.10.0
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/donseba/go-htmx"
+)
+
+type App struct {
+	htmx *htmx.HTMX
+}
+
+func main() {
+	// new app with htmx instance
+	app := &App{
+		htmx: htmx.New(),
+	}
+
+	mux := http.NewServeMux()
+	// wrap the htmx example middleware around the http handler
+	mux.Handle("/", http.HandlerFunc(app.Home))
+
+	err := http.ListenAndServe(":3000", mux)
+	log.Fatal(err)
+}
+
+func (a *App) Home(w http.ResponseWriter, r *http.Request) {
+	// initiate a new htmx handler
+	h := a.htmx.NewHandler(w, r)
+
+	// check if the request is a htmx request
+	if h.IsHxRequest() {
+		// do something
+	}
+
+	// check if the request is boosted
+	if h.IsHxBoosted() {
+		// do something
+	}
+
+	// check if the request is a history restore request
+	if h.IsHxHistoryRestoreRequest() {
+		// do something
+	}
+
+	// check if the request is a prompt request
+	if h.RenderPartial() {
+		// do something
+	}
+
+	// set the headers for the response, see docs for more options
+	h.PushURL("http://push.url")
+	h.ReTarget("#ReTarged")
+
+	// write the output like you normally do.
+	// check the inspector tool in the browser to see that the headers are set.
+	_, _ = h.Write([]byte("OK"))
+}
