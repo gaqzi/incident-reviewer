cff64ea26e71b71cb5715a9c9a79a68fb9c11bca
Björn Andersson <ba@sanitarium.se>
2024-11-21 00:45:30 +0800

    Run database migration using goose
    Because I liked the versioning system with fix, even if I'm wondering
    if the number of dependencies I'm pulling in will be worth it 😅

--- test/postgres.go
+++ test/postgres.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/go-sqlx/sqlx"
+	"github.com/pressly/goose/v3"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -37,6 +39,10 @@ func StartPostgres(ctx context.Context) (err error, conn string, done func()) {
 		return fmt.Errorf("failed to get postgres connection string: %w", err), "", nil
 	}
 
+	if err := migrate(connectionString); err != nil {
+		return err, "", func() {}
+	}
+
 	return nil, connectionString, func() {
 		// the below line is for the next version of testcontainers, but it was already part of the docs,
 		// so should move there when I upgrade.
@@ -45,4 +51,34 @@ func StartPostgres(ctx context.Context) (err error, conn string, done func()) {
 			log.Printf("failed to terminate container: %s", err)
 		}
 	}
+}
+
+type gooseErrorLogger struct{}
+
+func (*gooseErrorLogger) Fatalf(format string, v ...interface{}) { log.Fatalf(format, v...) }
+func (*gooseErrorLogger) Printf(format string, v ...interface{}) { /* noop */ }
+
+// migrate the database using goose.
+// Because we're using goose like this we pull in all of its dependencies
+// indirectly, which are a few of them, but since it's only used in the test
+// package we should be fine and not leak out.
+func migrate(conn string) error {
+	db, err := sqlx.Connect("postgres", conn)
+	if err != nil {
+		return fmt.Errorf("failed to connect to the DB: %w", err)
+	}
+	defer (func() { _ = db.Close() })()
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		return fmt.Errorf("failed to configure goose for postgres: %w", err)
+	}
+
+	// Don't output stuff when it's working as expected
+	goose.SetLogger(&gooseErrorLogger{})
+
+	if err := goose.Up(db.DB, "../migrations"); err != nil {
+		return fmt.Errorf("failed to migrate using goose: %w", err)
+	}
+
+	return nil
 }
