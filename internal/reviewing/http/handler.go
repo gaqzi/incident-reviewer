39e79262fbd70d66e42259db5a673c46b191bb52
Björn Andersson <ba@sanitarium.se>
2024-12-10 22:16:37 +0800

    Make the edit work
    I'm only relying on posting and getting back what I need from the
    client and I'm not providing any type of override protection, I need to
    add something that will check if we're trying to update the same
    version of the resource so I know it's safe to save

--- internal/reviewing/http/handler.go
+++ internal/reviewing/http/handler.go
@@ -3,8 +3,12 @@ package http
 import (
 	"context"
 	"embed"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/donseba/go-htmx"
@@ -13,6 +17,7 @@ import (
 	"github.com/gosimple/slug"
 
 	"github.com/gaqzi/incident-reviewer/internal/reviewing"
+	"github.com/gaqzi/incident-reviewer/internal/reviewing/storage"
 )
 
 var (
@@ -26,6 +31,25 @@ type App struct {
 	store   reviewing.Storage
 }
 
+// HTTPMethodFromFormField parses the _method to specify the method for the router.
+// https://github.com/go-chi/chi/issues/355#issuecomment-522699995
+func HTTPMethodFromFormField(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			switch strings.ToLower(r.PostFormValue("_method")) {
+			case "put":
+				r.Method = http.MethodPut
+			case "patch":
+				r.Method = http.MethodPatch
+			case "delete":
+				r.Method = http.MethodDelete
+			default:
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func Handler(store reviewing.Storage) func(chi.Router) {
 	app := App{
 		htmx:    htmx.New(),
@@ -36,6 +60,12 @@ func Handler(store reviewing.Storage) func(chi.Router) {
 	return func(r chi.Router) {
 		r.Get("/", app.Index)
 		r.Post("/", app.Create)
+		r.Get("/{id}-{slug}", app.Show)
+
+		r.Route("/{id}", func(r chi.Router) {
+			r.Get("/edit", app.Edit)
+			r.Post("/edit", app.Update)
+		})
 	}
 }
 
@@ -52,8 +82,8 @@ type ReviewBasic struct {
 	Description string `form:"description"`
 	Impact      string `form:"impact"`
 
-	UpdatedAt time.Time
-	CreatedAt time.Time
+	UpdatedAt time.Time `form:"updated_at"`
+	CreatedAt time.Time `form:"created_at"`
 }
 
 func (a *App) Create(w http.ResponseWriter, r *http.Request) {
@@ -116,7 +146,12 @@ func (a *App) renderIndex(h *htmx.Handler, r *http.Request, data map[string]any)
 		FS(templates).
 		SetData(data).
 		AddTemplateFunction("slug", slug.Make).
-		With(htmx.NewComponent("templates/_new.html").FS(templates), "New").
+		With(
+			htmx.NewComponent("templates/_new.html").
+				FS(templates).
+				Attach("templates/_review-fields.html"),
+			"New",
+		).
 		Wrap(baseContent(), "Body")
 
 	_, err := h.Render(r.Context(), page)
@@ -125,6 +160,148 @@ func (a *App) renderIndex(h *htmx.Handler, r *http.Request, data map[string]any)
 		h.WriteHeader(http.StatusInternalServerError)
 		_, _ = h.WriteString("failed to render")
 	}
+}
+
+func (a *App) Show(w http.ResponseWriter, r *http.Request) {
+	h := a.htmx.NewHandler(w, r)
+
+	reviewID, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
+	if err != nil {
+		slog.Error("failed to parse id for show", "id", r.PathValue("id"), "error", err)
+		h.WriteHeader(http.StatusBadRequest)
+		h.JustWriteString("invalid id")
+		return
+	}
+
+	review, err := a.store.Get(r.Context(), reviewID)
+	if err != nil {
+		slog.Info("get review error", "error", err)
+
+		var notFoundError *storage.NoReviewError
+		if errors.As(err, &notFoundError) {
+			h.WriteHeader(http.StatusNotFound)
+			h.JustWriteString(fmt.Sprintf("404: review by id '%d' not found.", reviewID))
+			return
+		}
+		slog.Error("error finding review", "error", err)
+	}
+
+	data := map[string]any{
+		"Review": review,
+	}
+
+	page := htmx.NewComponent("templates/show.html").
+		FS(templates).
+		SetData(data).
+		AddTemplateFunction("slug", slug.Make).
+		Wrap(baseContent(), "Body")
+
+	_, err = h.Render(r.Context(), page)
+	if err != nil {
+		slog.Error("failed to render", "error", err)
+		h.WriteHeader(http.StatusInternalServerError)
+		_, _ = h.WriteString("failed to render")
+	}
+}
+
+func (a *App) Edit(w http.ResponseWriter, r *http.Request) {
+	h := a.htmx.NewHandler(w, r)
+
+	reviewID, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
+	if err != nil {
+		slog.Error("failed to parse id for show", "id", r.PathValue("id"), "error", err)
+		h.WriteHeader(http.StatusBadRequest)
+		h.JustWriteString("invalid id")
+		return
+	}
+
+	review, err := a.store.Get(r.Context(), reviewID)
+	if err != nil {
+		slog.Info("get review error", "error", err)
+
+		var notFoundError *storage.NoReviewError
+		if errors.As(err, &notFoundError) {
+			h.WriteHeader(http.StatusNotFound)
+			h.JustWriteString(fmt.Sprintf("404: review by id '%d' not found.", reviewID))
+			return
+		}
+		slog.Error("error finding review", "error", err)
+	}
+
+	data := map[string]any{
+		"Review": review,
+	}
+
+	page := htmx.NewComponent("templates/edit.html").
+		FS(templates).
+		SetData(data).
+		Attach("templates/_review-fields.html").
+		AddTemplateFunction("slug", slug.Make).
+		Wrap(baseContent(), "Body")
+
+	_, err = h.Render(r.Context(), page)
+	if err != nil {
+		slog.Error("failed to render", "error", err)
+		h.WriteHeader(http.StatusInternalServerError)
+		_, _ = h.WriteString("failed to render")
+	}
+
+}
+
+func (a *App) Update(w http.ResponseWriter, r *http.Request) {
+	h := a.htmx.NewHandler(w, r)
+
+	reviewID, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
+	if err != nil {
+		slog.Error("failed to parse id for show", "id", r.PathValue("id"), "error", err)
+		h.WriteHeader(http.StatusBadRequest)
+		h.JustWriteString("invalid id")
+		return
+	}
+
+	review, err := a.store.Get(r.Context(), reviewID)
+	if err != nil {
+		slog.Info("get review error", "error", err)
+
+		var notFoundError *storage.NoReviewError
+		if errors.As(err, &notFoundError) {
+			h.WriteHeader(http.StatusNotFound)
+			h.JustWriteString(fmt.Sprintf("404: review by id '%d' not found.", reviewID))
+			return
+		}
+		slog.Error("error finding review", "error", err)
+	}
+
+	if err := r.ParseForm(); err != nil {
+		slog.Error("failed to parse form", "error", err)
+		h.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var inc ReviewBasic
+	if err := a.decoder.Decode(&inc, r.Form); err != nil {
+		slog.Error("failed to decode basic incident form", "error", err)
+		h.WriteHeader(http.StatusBadRequest)
+		h.JustWriteString(err.Error())
+		return
+	}
+
+	// Update all the fields that can change. Note: We don't change CreatedAt
+	review.URL = inc.URL
+	review.Title = inc.Title
+	review.Description = inc.Description
+	review.Impact = inc.Impact
+
+	_, err = a.store.Save(r.Context(), review)
+	if err != nil {
+		slog.Error("failed to save review", "id", reviewID, "error", err)
+		h.WriteHeader(http.StatusInternalServerError)
+		h.JustWriteString(err.Error())
+		return
+	}
+
+	h.Header().Add("Location", fmt.Sprintf("/reviews/%d-%s", reviewID, slug.Make(inc.Title)))
+	h.WriteHeader(http.StatusSeeOther)
 }
 
 func baseContent() htmx.RenderableComponent {
